c0e027fae77afa949693f6b9d3623cb9068b3fb1
fabienbodin <fabien.bodin@epsi.fr>
2021-02-02 14:15:44 +0100

    Add Vuetify

---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app-bar color="deep-purple accent-4" dark app>
+    <v-toolbar-title>Gestion des utilisateurs</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn icon @click="openAddEdit()">
+      <v-icon>mdi-plus</v-icon>
+    </v-btn>
+    <!--<template v-slot:extension>
+      <v-tabs align-with-title>
+        <v-tab>Tab 1</v-tab>
+        <v-tab>Tab 2</v-tab>
+        <v-tab>Tab 3</v-tab>
+      </v-tabs>
+    </template>-->
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
